58f514adf73aa1b07c978ec5286ea5fbc47b0162
Shashank <shashanksagar398@gmail.com>
2025-03-12 21:04:30 +0530

    feat: remove outdated workflow check comment from useBalance hook

--- packages/store/src/hooks/useBalance.ts
+++ packages/store/src/hooks/useBalance.ts
@@ -4,7 +4,6 @@ import { balanceAtom } from "../atoms/balance"
 
 export const useBalance = () => {
     //Same as redux useSelector
-    //workflow check
     const value = useRecoilValue(balanceAtom);
     return value;
 }
