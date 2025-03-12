1a324fedce1a01bff6ab95c010ded7e19d01098f
Shashank <shashanksagar398@gmail.com>
2025-03-12 20:46:39 +0530

    feat: add workflow check comment to useBalance hook

--- packages/store/src/hooks/useBalance.ts
+++ packages/store/src/hooks/useBalance.ts
@@ -4,6 +4,7 @@ import { balanceAtom } from "../atoms/balance"
 
 export const useBalance = () => {
     //Same as redux useSelector
+    //workflow check
     const value = useRecoilValue(balanceAtom);
     return value;
 }
